eb3422cf2a9319be75a3d2974ef4d00653929af7
wonderfulua <45660355+wonderfulua@users.noreply.github.com>
2021-07-08 23:29:58 +0800

    Add files via upload

---
+++ aaa_test.py
@@ -0,0 +1,12 @@
+# from os import times
+import pytest
+# from time import time
+class Testrun:
+    def test_ao(self):
+        # time.sleep(3)
+        print('right!')
+
+    @pytest.mark.usermanager
+    def test_ao1(self):
+        # time.sleep(3)
+        print('right!')
--- test_login.py
+++ test_login.py
@@ -5,18 +5,17 @@ class Testrun:
     def test_run(self):
         # time.sleep(3)
         print('right!')
+    @pytest.mark.run(order=1)
+    @pytest.mark.smoke
     def test_run1(self):
         # time.sleep(3)
         print('right!')
     def test_run2(self):
         # time.sleep(3)
         print('right!')
-        assert 1==2
+        # assert 1==2
+    
+    @pytest.mark.usermanager
     def test_run3(self):
         # time.sleep(3)
         print('right!')
-    
-
-if __name__ == '__main__':
-    # pytest.main(['-vs','-n=2'])
-    pytest.main(['-vs','--reruns=2'])
